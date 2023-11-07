046d0ab27942435a3a60194bf97cb021bc9496ef
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-11-07 18:38:34 +0800

    Added Folders but no content

---
+++ src/pages/Private/Records.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Records(props) {
+  return <div></div>;
+}
+
+export default Records;
---
+++ src/pages/Public/AboutUsPublic.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AboutUsPublic(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default AboutUsPublic;
