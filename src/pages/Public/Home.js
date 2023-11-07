6ff3bb5d67a4236246c4b9b66be12016f3e0cb8f
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-11-07 23:55:57 +0800

    Added new Component

--- src/pages/Public/Home.js
+++ src/pages/Public/Home.js
@@ -9,7 +9,6 @@ import InputText from "../../components/InputText";
 import InputSelect from "../../components/InputSelect";
 import axios from "axios";
 import "./../../components/Styles/TermsConditions.css";
-import TermsConditions from "../../components/TermsConditions";
 import {
   Button,
   Dialog,
@@ -18,6 +17,7 @@ import {
   DialogTitle,
   TextField,
 } from "@mui/material";
+import Message from "../../components/Message";
 
 function Home(props) {
   const navigation = useNavigate();
@@ -37,6 +37,14 @@ function Home(props) {
     EMAIL: "",
     STATUS: "",
   });
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -77,14 +85,6 @@ function Home(props) {
       setTerms(!terms);
       openModal();
     }
-  };
-
-  const openModal = () => {
-    setModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalOpen(false);
   };
 
   const handleSubmitMessage = async (e) => {
@@ -210,49 +210,7 @@ function Home(props) {
           >
             <DialogTitle>Terms of Services</DialogTitle>
             <DialogContent>
-              <p className="terms-agrement">
-                Clients have the option to choose between a 50% downpayment or
-                full upfront payment once the sketch is approved. Please note
-                that refunds or cancellations are not permitted after payment
-                has been made. The turnaround time for completing artwork is
-                dependent on the complexity of the requested art, and pricing
-                may vary accordingly. The finished artwork will be delivered via
-                email. Additional charges apply for extra characters at $5 or
-                Php 250, while flat backgrounds are provided free of charge,
-                with a $3 fee for detailed backgrounds. To ensure a smooth
-                process, it's essential to read and comprehend these terms of
-                service. Begin by contacting us via Twitter, Facebook, or
-                Instagram, and provide clear references along with a detailed
-                description of your request. Upon confirmation of all details,
-                you can proceed with either an upfront payment or a 50%
-                downpayment of the total price. We will commence work upon
-                payment receipt. Prior to coloring, rough sketches will be
-                submitted for your approval, with only one revision permitted.
-                We will keep you informed with periodic updates on the progress
-                of your commission. Once the commission is finalized, a PNG file
-                will be sent to you via email.
-              </p>
-              <p>
-                By clicking proceed you have agreed to the terms of services
-              </p>
-              <div>
-                <h3>You can follow and contact us on:</h3>
-                <a
-                  className="link"
-                  href="https://www.instagram.com/missartarchive/"
-                >
-                  <Instagram></Instagram>@missartarchive
-                </a>
-                <a
-                  className="link"
-                  href="https://web.facebook.com/MissArtCommission"
-                >
-                  <Facebook></Facebook>@missartcommission
-                </a>
-                <a className="link" href="https://twitter.com/MissARTarchive">
-                  <Twitter></Twitter>@missartarchive
-                </a>
-              </div>
+              <Message></Message>
             </DialogContent>
             <DialogActions>
               <Button onClick={closeModal} color="primary">
